a32728f9f96ad5e687a6f0c096be1826f7c6e25f
Marius R <Mrel0425@gmail.com>
2025-07-29 14:52:13 +0200

    Add weather icons and implement weather data fetching; update layout for last-trip and trip-logger sections

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 import { workouts } from "./workouts.js";
 console.log(workouts);
+const weatherUrl =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weather_code,is_day&forecast_hours=6";
+
+async function getWeatherData() {
+  let response = await fetch(weatherUrl);
+  if (!response.ok) {
+    console.error("error");
+  }
+  let weatherData = await response.json();
+  console.table(weatherData);
+  return weatherData;
+}
+
+getWeatherData();
